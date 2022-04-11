0eff3574d719b1f539ffb90573c864d841be6711
Rafael <rafaelmantovanipessoa@gmail.com>
2022-04-11 17:44:25 -0300

    Add a flag to a hung up call in order to be ignored when got from the queue and refactor free_operator function.

--- callcenter/CallCenter.py
+++ callcenter/CallCenter.py
@@ -2,5 +2,4 @@ from CommandInterpreter import CommandInterpreter
 from callcenter.LocalCallCenterManager import LocalCallCenterManager
 
 if __name__ == '__main__':
-
     CommandInterpreter(LocalCallCenterManager()).cmdloop()
--- callcenter/LocalCallCenterManager.py
+++ callcenter/LocalCallCenterManager.py
@@ -24,14 +24,17 @@ class LocalCallCenterManager(CallCenterManager):
             heapq.heapify(self.__operators_heapq)
             print(f"Call {call_id} ringing for operator {most_available_operator.id}")
 
-    def __free_operator(self, operator, post_free_msg):
+    def __assign_waiting_call(self):
+        while not self.__callQueue.empty():
+            call_id = self.__callQueue.get()
+            if self.__current_calls_assignment.get(call_id) != "hung up":
+                self.__assign_call(call_id)
+                break
+
+    def __free_operator(self, operator):
         operator.status = OperatorStatus.AVAILABLE
-        del self.__current_calls_assignment[operator.current_call_id]
         operator.current_call_id = None
         heapq.heapify(self.__operators_heapq)
-        print(post_free_msg)
-        if self.__callQueue.not_empty:
-            self.__assign_call(self.__callQueue.get())
 
     def receive_call(self, call_id):
         print(f"Call {call_id} received")
@@ -52,20 +55,26 @@ class LocalCallCenterManager(CallCenterManager):
     def reject_call(self, operator_id):
         operator = self.__operators_dict.get(operator_id)
         if operator.status == OperatorStatus.RINGING:
-            msg = f"Call {operator.current_call_id} rejected by operator {operator_id}"
-            self.__free_operator(operator, msg)
-            heapq.heapify(self.__operators_heapq)
+            rejected_call = operator.current_call_id
+            del self.__current_calls_assignment[rejected_call]
+            self.__free_operator(operator)
+            self.__assign_call(rejected_call)
+            print(f"Call {rejected_call} rejected by operator {operator_id}")
+            self.__assign_waiting_call()
         else:
             print(f"There is nothing for operator {operator_id} to reject")
 
     def hang_up_call(self, call_id):
         operator_on_call = self.__current_calls_assignment.get(call_id)
+        self.__current_calls_assignment[call_id] = "hung up"
         msg = f"Call {call_id} missed"
 
         if isinstance(operator_on_call, Operator):
             if operator_on_call.status == OperatorStatus.BUSY:
                 msg = f"Call {call_id} finished and operator {operator_on_call.id} available"
-            self.__free_operator(operator_on_call, msg)
-            return
 
-        print(msg)
+            self.__free_operator(operator_on_call)
+            print(msg)
+            self.__assign_waiting_call()
+        else:
+            print(msg)
